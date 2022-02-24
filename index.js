c1ca010c9993c2d422d6419c3969f5a58e9e744d
pruizj <45947633+pruizj@users.noreply.github.com>
2022-02-24 16:07:58 +0000

    No permitir mandar mensaje si no estas logeado.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
@@ -15,7 +16,7 @@ io.on('connection', (socket) => {
   socket.on('log in', usr => {
     io.emit('log in', usr);
   });
-
+  
 });
 
 http.listen(port, () => {
