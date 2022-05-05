dc654778607a16d6d73634bc47abc2668f60d557
pruizj <45947633+pruizj@users.noreply.github.com>
2022-05-05 15:08:47 +0000

    Show image user next to the nickname in the register when the user log in

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
@@ -26,4 +25,3 @@ io.on('connection', (socket) => {
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
-
